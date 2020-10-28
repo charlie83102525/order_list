d8b979063f8686a8c1f27791c5e5a9eb4f16db6e
Charlie Tsai <charlie83102525@gmail.com>
2020-10-28 23:03:08 +0800

    fix

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ def handle_message(event):
 
 
     #for ol in order_list:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=r))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
