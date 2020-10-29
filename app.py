44e27162a8d6844246e21541e894ac20a3f08e77
Charlie Tsai <charlie83102525@gmail.com>
2020-10-29 21:17:57 +0800

    fix

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def handle_message(event):
     #for ol in order_list:
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=a))
+        TextSendMessage(text=r))
 
 
 
